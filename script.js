26b48457e2462d9988f65647df553b2493dff13d
luketucich <ltucich@gmail.com>
2024-09-03 17:40:31 -0400

    Initializes project with skeleton HTML/CSS and JS files.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Initalizes library to store user's collection of books
+const userLibrary = [];
+
+// Object constructor for creating a new book
+function Book(author, title, pages, hasRead) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.hasRead = hasRead
+};
+
+// 
+function addBookToLibrary() {
+  // do stuff here
+}
