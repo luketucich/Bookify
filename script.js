0c50a7f376af12d4af9518ffa93e8d7ca14c8bfd
luketucich <ltucich@gmail.com>
2024-09-05 15:35:30 -0400

    Adding a book through the modal will now correctly display in the user's library. Additionally, the search bar and submit form button have now been merged. Some CSS styles have been changed for more fluid animations that don't disrupt surrounding elements.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Initalizes library to store user's collection of books
 const userLibrary = [];
 const libraryContainer = document.getElementById('booksContainer');
+const mainForm = document.getElementById('mainForm');
+const removeBookButtons = document.getElementsByClassName('libraryRemoveBook');
 var haveAddedBooks = false;
 
  // Object constructor for creating a new book
@@ -14,6 +16,8 @@ function addBook(author, title, pages) {
 document.getElementById('mainForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting normally
     
+    mainForm.style.display = 'none';
+
     // Sets modal's form text to be user's input for the book title
     var bookTitle = document.getElementById('mainFormInput').value;
     document.getElementById('bookTitle').textContent = bookTitle;
@@ -27,14 +31,17 @@ document.getElementById('mainForm').addEventListener('submit', function(event) {
 document.getElementById('closeModal').addEventListener('click', function() {
     var modal = document.getElementById('modal');
     modal.style.display = 'none';
+    mainForm.style.display = 'flex';
     document.getElementById('mainForm').reset();
     document.getElementById('modalForm').reset();
 });
 
 // Adds book to library
 function addBookToLibrary(){
-    // Grabs current book's title
+    // Grabs current book's information
     var bookTitle = document.getElementById('bookTitle').textContent;
+    var bookAuthor = document.getElementById('bookAuthor').value;
+    var bookPageCount = document.getElementById('bookPageCount').value;
 
     // Removes placeholder book elements
     if(haveAddedBooks == false){
@@ -46,11 +53,41 @@ function addBookToLibrary(){
     // Updates haveAddedBooks value so placeholder books are no longer being attempted to remove
     haveAddedBooks = true; 
 
-    // Adds book to library
-    const div = document.createElement('div');
-    div.classList.add('book');
-    div.textContent = bookTitle;
-    libraryContainer.appendChild(div);
+    // Adds book to library and updates DOM
+    const libraryBook = document.createElement('div');
+    libraryBook.classList.add('book');
+
+    bookInfo = [bookTitle,bookAuthor,bookPageCount] // Current book's info
+    libraryBookIDs = ['libraryBookTitle', 'libraryBookAuthor', 'libraryBookPageCount']; // Library book IDs for styling
+
+    // Adds information to libraryBook with corresponding IDs for styling (title, author, page count)
+    for (let i = 0 ; i < 3 ; i++){
+
+        // Creates the library book div
+        var libraryBookInfo = document.createElement('div');
+        libraryBookInfo.setAttribute('id', libraryBookIDs[i]);
+        
+        if (i == 0){
+            libraryBookInfo.textContent = bookInfo[i] // add title text content 
+            libraryBook.appendChild(libraryBookInfo); // add info to library book
+
+        } else if (i == 1){
+            libraryBookInfo.textContent = bookInfo[i]; // add author text content
+
+            // Create section in library book dedicated to book's info (author, pages, etc.)
+            var libraryBookDescContainer = document.createElement('div');
+            libraryBookDescContainer.setAttribute('id', 'libraryBookDescContainer');
+            libraryBook.appendChild(libraryBookDescContainer);
+            libraryBookDescContainer.appendChild(libraryBookInfo); // adds book info to dedicated section
+
+        } else if (i == 2){
+            libraryBookInfo.textContent = bookInfo[i] + ' pages'; // add page text content
+            libraryBookDescContainer.appendChild(libraryBookInfo); // adds book info to dedicated section
+        }
+    }
+
+    // Adds libraryBook to user's library
+    libraryContainer.appendChild(libraryBook);
 };
 
 // Submits modal form, adding book to user library, and resets forms
@@ -66,8 +103,10 @@ document.getElementById('modalForm').addEventListener('submit', function(event) 
     // Adds book to library
     addBook(bookAuthor, bookTitle, bookPageCount);
     addBookToLibrary();
+
+    // Updates modal display to none and resets forms
     modal.style.display = 'none';
+    mainForm.style.display = 'flex';
     document.getElementById('mainForm').reset();
     document.getElementById('modalForm').reset();
 });
-
