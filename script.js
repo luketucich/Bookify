4a3f42af813a4a0c318b9aee72bfbc383197460d
luketucich <ltucich@gmail.com>
2024-09-07 14:33:16 -0400

    Finished all styling and functionality of the program. Users can now indicate the 'read' status of their books with a toggle. Additionally, minor HTML/CSS tweaks to ensure class & id consistency.

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const libraryContainer = document.getElementById('booksContainer');
 const mainForm = document.getElementById('mainForm');
 const modal = document.getElementById('modal');
 const booksMessage = document.getElementById('booksMessage');
+const main = document.getElementById('main');
 
 let haveAddedBooks = false;
 
  // Object constructor for creating a new book
-function addBook(author, title, pages) {
+function addBook(author, title, pages, read) {
     this.author = author
     this.title = title
     this.pages = pages
+    this.read = read
 
 };
 
@@ -19,8 +21,8 @@ function addBook(author, title, pages) {
 document.getElementById('mainForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting normally
     
-    // Sets main form display to none so it doesn't overlap on page
-    mainForm.style.display = 'none';
+    // Sets main  display to none so it doesn't overlap on page
+    main.style.display = 'none';
 
     // Sets modal's form text to be user's input for the book title
     const bookTitle = document.getElementById('mainFormInput').value;
@@ -33,7 +35,7 @@ document.getElementById('mainForm').addEventListener('submit', function(event) {
 // Closes modal and resets forms
 document.getElementById('closeModal').addEventListener('click', function() {
     modal.style.display = 'none';
-    mainForm.style.display = 'flex';
+    main.style.display = 'flex';
     document.getElementById('mainForm').reset();
     document.getElementById('modalForm').reset();
 });
@@ -45,7 +47,8 @@ function addBookToArray(){
     const bookAuthor = document.getElementById('bookAuthor').value;
     const bookTitle = document.getElementById('bookTitle').textContent;
     const bookPageCount = document.getElementById('bookPageCount').value;
-    const newBook = new addBook(bookAuthor, bookTitle, bookPageCount);
+    const bookRead = document.getElementById('bookRead').checked;
+    const newBook = new addBook(bookAuthor, bookTitle, bookPageCount, bookRead);
 
     // Adds book to library
     userLibrary.push(newBook);
@@ -66,6 +69,7 @@ function addBookToDOM(){
     const currentBookAuthor = userLibrary[currentBookIndex].author;
     const currentBookTitle = userLibrary[currentBookIndex].title;
     const currentBookPageCount = userLibrary[currentBookIndex].pages;
+    const currentBookRead = userLibrary[currentBookIndex].read;
 
     // Creates visual for current book in the user's library
     // Book's card
@@ -76,29 +80,45 @@ function addBookToDOM(){
     // Card's title
     const bookCardTitle = document.createElement('div');
     bookCardTitle.textContent = currentBookTitle;
-    bookCardTitle.setAttribute('id', 'libraryBookTitle');
+    bookCardTitle.className = 'libraryBookTitle';
     bookCardContainer.appendChild(bookCardTitle);
 
     // Card's content container
     const bookCardContent = document.createElement('div');
-    bookCardContent.setAttribute('id', 'libraryBookContent');
+    bookCardContent.className = 'libraryBookContent';
     bookCardContainer.appendChild(bookCardContent);
 
-    // Card's content (author, pages, etc)
+    // Card's content (author, pages)
         // Author
     const bookCardAuthor = document.createElement('div');
     bookCardAuthor.textContent = currentBookAuthor;
-    bookCardAuthor.setAttribute('id', 'libraryBookAuthor');
+    bookCardAuthor.className = 'libraryBookAuthor';
     bookCardContent.appendChild(bookCardAuthor);
         // Page count
     const bookCardPageCount = document.createElement('div');
     bookCardPageCount.textContent = currentBookPageCount + ' pages';
-    bookCardPageCount.setAttribute('id', 'libraryBookPageCount');
+    bookCardPageCount.className = 'libraryBookPageCount';
     bookCardContent.appendChild(bookCardPageCount);
-        // Remove button
+    
+    // Card's icon container
+    const bookCardIcons = document.createElement('div');
+    bookCardIcons.className = 'libraryIconsContainer';
+    bookCardContainer.appendChild(bookCardIcons);
+
+    // Card's icons (read, trash)
+        // Read
+    const readIcon = document.createElement('i');
+    if (currentBookRead == true){
+        readIcon.className = 'animatedButton fa-solid fa-bookmark';
+        readIcon.style.color = "#8B0000";
+    } else {
+        readIcon.className = 'animatedButton fa-regular fa-bookmark';
+    }
+    bookCardIcons.appendChild(readIcon);
+        // Trash
     const removeIcon = document.createElement('i');
-    removeIcon.className = 'fa-solid fa-trash-can'; 
-    bookCardContent.appendChild(removeIcon);
+    removeIcon.className = 'animatedButton fa-regular fa-trash-can';
+    bookCardIcons.appendChild(removeIcon);
 };
 
 // Submits modal form, adding book to user library, and resets forms
@@ -112,7 +132,7 @@ document.getElementById('modalForm').addEventListener('submit', function(event) 
     // Updates element displays and resets forms
     booksMessage.style.display = 'none';
     modal.style.display = 'none';
-    mainForm.style.display = 'flex';
+    main.style.display = 'flex';
     document.getElementById('mainForm').reset();
     document.getElementById('modalForm').reset();
 
@@ -138,6 +158,37 @@ document.getElementById('booksContainer').addEventListener('click', function(eve
             booksMessage.style.display = 'block';
         }
     }
-
 });
 
+// Allows user to update book's read value and styling
+document.getElementById('booksContainer').addEventListener('click', function(event){
+    if(event.target && event.target.matches('.fa-bookmark')) {
+
+        // Creates a list of books in the DOM 
+        const libraryBooksInDOM = Array.from(libraryContainer.children);
+
+        // Selects the current book container element and index
+        const selectedLibraryBook = event.target.parentElement.parentElement;
+        const selectedLibraryBookIndex = libraryBooksInDOM.indexOf(selectedLibraryBook);
+
+        // Changes read value in userLibrary and updates styling accordingly
+        if (event.target.classList == 'animatedButton fa-solid fa-bookmark'){
+            event.target.classList = 'animatedButton fa-regular fa-bookmark';
+            event.target.style.color = 'rgb(102, 102, 102)';
+        } else{
+            event.target.classList = 'animatedButton fa-solid fa-bookmark';
+            event.target.style.color = '#8B0000';
+        };
+        
+        // Read value will only be updated on non-placeholder books
+        try {
+            if(userLibrary[selectedLibraryBookIndex].read == true){
+                userLibrary[selectedLibraryBookIndex].read = false;
+            } else{
+                userLibrary[selectedLibraryBookIndex].read = true;
+            };
+        } catch (error) {
+            console.log("This is a placeholder book.");
+        }
+    }
+});
